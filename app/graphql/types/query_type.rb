4a22064ed7b65122889d5c4c1dfd840e87a27968
Hussnain Younas <FindHussnain@gmail.com>
2022-04-05 14:25:34 +0500

    Created Queries for accessing single note with id and all notes

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,6 +3,8 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
+    field :fetch_notes, resolver: Queries::FetchNotes
+    field :fetch_note, resolver: Queries::FetchNote
     # TODO: remove me
     field :test_field, String, null: false,
       description: "An example field added by the generator"
